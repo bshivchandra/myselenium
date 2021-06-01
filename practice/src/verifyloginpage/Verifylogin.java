4c632541e5fb9750ae3c452b9e68fb8d8aec8fda
shiv <bshivchandra>
2021-06-01 20:29:44 +0530

    pom page factory

--- practice/src/verifyloginpage/Verifylogin.java
+++ practice/src/verifyloginpage/Verifylogin.java
@@ -3,28 +3,43 @@ package verifyloginpage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Verifylogin {
-	WebDriver driver;
-	By username = By.xpath("//*[@id=\"loginid\"]");
-	By password = By.xpath("//*[@id=\"loginpass\"]");
-	By loginbutton = By.xpath("//*[@id=\"loginuser\"]");
 	
+	    WebDriver driver;
+
 	public Verifylogin(WebDriver driver)
 	{ 
 		this.driver=driver;
+		PageFactory.initElements(driver, this);
 	}
+	
+	@FindBy(id="loginid")
+	WebElement username;
+	
+	@FindBy(id="loginpass")
+	WebElement password;
+
+	@FindBy(id="loginuser")
+	WebElement loginbutton;
+
+	
+	
 	public void loginIdPass(String userid, String pass) throws InterruptedException
 	{
-		driver.findElement(By.xpath("//*[@id=\"navbarCollapse\"]/ul[2]/li/a")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"navbarCollapse\"]/ul[2]/li/a")).click();	
 		JavascriptExecutor js=(JavascriptExecutor)driver;
 		js.executeScript("window.scrollBy(0,500)");
-    	driver.findElement(username).sendKeys(userid);
-		driver.findElement(password).sendKeys(pass);
-		driver.findElement(loginbutton).click();		
+    	username.sendKeys(userid);
+		password.sendKeys(pass);
+		loginbutton.click();		
 		System.out.println(driver.findElement(By.className("modal-body")).getText());
 		Thread.sleep(1000);
-		driver.findElement(By.cssSelector("#errormodal>div>div>div.modal-footer > button")).click();
+		driver.findElement(By.className("close")).click();
 		Thread.sleep(1000);
 
 		driver.quit();
